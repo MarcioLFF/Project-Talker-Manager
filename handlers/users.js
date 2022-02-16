c22d3ff1c78942bff722cd4947f7c9cb6d05cc80
Marcio <marciofonseca.ufrj@gmail.com>
2022-02-16 02:11:52 -0300

    Ajustando lint e testando req 1

--- handlers/users.js
+++ handlers/users.js
@@ -1,15 +1,16 @@
 const fs = require('fs/promises');
+
 const fileName = 'talker.json';
 
 module.exports = async (req, res, _next) => {
-    try{
-        const data = await fs.readFile(fileName);
-				const datajson = JSON.parse(data)
-				if (datajson.length===0) {
-					return res.status(200).send(datajson)
-				}
-         return res.status(200).send(datajson);
-      } catch (e) {
-					res.status(500).send({message:'erro na leitura do arquivo'})
-      }
-    };
+try {
+const data = await fs.readFile(fileName);
+const datajson = JSON.parse(data);
+if (datajson.length === 0) {
+return res.status(200).send(datajson);
+}
+  return res.status(200).send(datajson);
+} catch (e) {
+res.status(500).send({ message: 'erro na leitura do arquivo' });
+}
+};
