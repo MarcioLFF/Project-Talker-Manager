207874b85bb49520b8839860e392a6413d56e11a
Marcio <marciofonseca.ufrj@gmail.com>
2022-02-16 02:02:20 -0300

    Corrigindo requisito 1

--- handlers/users.js
+++ handlers/users.js
@@ -1,8 +1,15 @@
 const fs = require('fs/promises');
-
 const fileName = 'talker.json';
 
 module.exports = async (req, res, _next) => {
+    try{
         const data = await fs.readFile(fileName);
-        res.status(200).send(data);
-      };
+				const datajson = JSON.parse(data)
+				if (datajson.length===0) {
+					return res.status(200).send(datajson)
+				}
+         return res.status(200).send(datajson);
+      } catch (e) {
+					res.status(500).send({message:'erro na leitura do arquivo'})
+      }
+    };
